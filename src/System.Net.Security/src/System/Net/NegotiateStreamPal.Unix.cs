64519b31952d265c6ef3813b19eb43caa8c59c66
kapilash <kapilash@outlook.com>
2016-03-02 16:43:43 +0530

    Asserting for the type of security buffer

--- src/System.Net.Security/src/System/Net/NegotiateStreamPal.Unix.cs
+++ src/System.Net.Security/src/System/Net/NegotiateStreamPal.Unix.cs
@@ -92,6 +92,7 @@ namespace System.Net.Security
             SecurityBuffer outSecurityBuffer,
             ref ContextFlagsPal contextFlags)
         {
+            Debug.Assert(inSecurityBufferArray == null || inSecurityBufferArray[0].type == SecurityBufferType.Token, "First buffer is a token");
             // TODO (Issue #3718): The second buffer can contain a channel binding which is not supported
             if ((null != inSecurityBufferArray) && (inSecurityBufferArray.Length > 1))
             {
